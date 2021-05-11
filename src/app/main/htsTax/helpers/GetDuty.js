62b70244de9926152adaaaa232215d6f610e8806
Fernando Guerra <fernandoguerra@MacBook-Air-de-Fernando.local>
2021-05-10 20:41:51 -0400

    perro

---
+++ src/app/main/htsTax/helpers/GetDuty.js
@@ -0,0 +1,26 @@
+export const GetDuty = async (htsnumber, origincountry, destinationcountry) => {
+
+    console.log(htsnumber);
+
+    var requestOptions = {
+
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+           
+            "HSCode": htsnumber,
+             "OriginCountryCode": origincountry,
+            "DestinationCountryCode": destinationcountry
+        })
+    };
+
+    const resp = await fetch(`https://dx5kwxo8qa.execute-api.us-west-2.amazonaws.com/dev/getduty`, requestOptions);
+    const data = await resp.json();
+    const dato = data.body;
+    //console.log("DATO:");
+    //console.log(dato);
+
+
+    return dato;
+
+}
