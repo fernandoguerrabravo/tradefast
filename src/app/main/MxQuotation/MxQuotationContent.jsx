26969b611157729b8248381348aa9ecf9d2616b8
Fernando Guerra <fernandoguerra@iMac-de-Fernando.local>
2021-10-25 18:17:47 -0300

    quotation

---
+++ src/app/main/MxQuotation/MxQuotationContent.jsx
@@ -0,0 +1,12 @@
+import { memo } from 'react';
+import MxQuotationApp from './MxQuotationapp';
+
+const MxQuotationContent = () => {
+	return (
+		<>
+			<MxQuotationApp />
+		</>
+	);
+};
+
+export default memo(MxQuotationContent);
